631bd0ada4e9f2cdb4d153e2e6b4432a005f9437
mohammedanaz <mohammedanaz@gmail.com>
2024-04-30 12:18:05 +0530

    User list in the custom admin page block/ublock feature created asynchronously

--- myfab/custom_admin/views.py
+++ myfab/custom_admin/views.py
@@ -15,19 +15,15 @@ class AdminHome(View):
         return render(request, 'admin_home.html', context)
     
     def post(self, request):
-        print("Post data is:", request.body)
         json_data = json.loads(request.body)
-        print("json data is:", json_data)
 
         if 'user_id' in json_data:  # Check for user_id in json data
                 user_id = json_data.get('user_id')
-                is_active = json_data.get('is_active') == 'on'  # Convert to boolean
-                print("user_id and Is_active from json data is:", user_id, is_active)
+                is_active = json_data.get('is_active')
                 try:
                     user = CustomUser.objects.get(id=user_id)
                     user.is_active = is_active
                     user.save()
-                    print("New status saved  for the user is", user.is_active)
                     return JsonResponse({'success': True})
                 except CustomUser.DoesNotExist:
                     return JsonResponse({'success': False, 'error': 'User not found'})

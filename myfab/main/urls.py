bc9acd8b5c419ad262d9723ccf9bc4247ff3eee0
mohammedanaz <mohammedanaz@gmail.com>
2024-05-01 10:51:10 +0530

    home page for women created ...only shop by usage changed

--- myfab/main/urls.py
+++ myfab/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, Product
+from .views import Home, Product, HomeWomen
 
 urlpatterns = [
     path('', Home.as_view(), name = 'home'),
     path('main/', Home.as_view(), name = 'home'),
+    path('main/women/', HomeWomen.as_view(), name = 'home_women'),
     path('main/products/', Product.as_view(), name = 'products'),
 ]
--- myfab/main/views.py
+++ myfab/main/views.py
@@ -8,7 +8,7 @@ from .models import Usage, Category, CategoryChoice
 
 
 
-############################### Home CBV ######################################
+############################### Home Men CBV ######################################
 
 class Home(View):
     def get(self, request):
@@ -31,6 +31,32 @@ class Home(View):
                    }
 
         return render(request, 'home.html', context)
+    
+
+
+############################### Home Women CBV ######################################
+
+class HomeWomen(View):
+    def get(self, request):
+        
+        usages = Usage.objects.filter(gender='F')  # Filter usages with male gender
+        usage_list = list(usages)
+
+        categories_without_img = Category.objects.filter(categorychoice__image__isnull=False).distinct()
+        categories_filtered = categories_without_img.exclude(name='gender')
+        choices_without_img = CategoryChoice.objects.filter(image__isnull=False)
+        choices_filtered = choices_without_img.exclude(choices = 'male')
+        dict = {}
+        for category in categories_filtered:
+            choices = choices_filtered.filter(category = category)
+            dict[category.name] = choices
+        context = {'usages': usage_list,
+                   'categories_filtered': categories_filtered,
+                   'choices_filtered': choices_filtered,
+                   'dict': dict
+                   }
+
+        return render(request, 'home_women.html', context)
     
 
 ############################### Products CBV ######################################

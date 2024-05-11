fe3fbfbc18a7ec01fb8f264be0829c8469e13126
mohammedanaz <mohammedanaz@gmail.com>
2024-05-11 19:27:05 +0530

    add to cart function(form data to view) successfull with axios.....view logic to add cart items to DB pending

--- myfab/orders/views.py
+++ myfab/orders/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views import View
+from main.models import Product
+from django.http import JsonResponse
+import json
 
 ########################## Cart View #################################
 class Cart(View):
 
     def post(self, request):
-        product_id = request.POST.get('product_id')
+        # Parse the JSON data sent from the frontend
+        data = json.loads(request.body)
 
-        print('product_id: ', product_id)
+        # Extract relevant fields from the data
+        user = request.user
+        product_id = data.get('product_id')
+        product = Product.objects.get(pk=product_id)
+        ordertype = data.get('ordertype')
+        dresstype = data.get('dresstype')
 
-        return redirect(reverse('main:products'))
+        print('product: ', product,
+              'user: ', user,
+              'ordertype :', ordertype,
+              'dresstype :', dresstype
+              )
+
+        return JsonResponse({'message': 'Data received successfully'})
     
-    def get(self, request):
-        return render(request, 'cart.html')

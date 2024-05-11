bc3601132cf31e96275dc0fa639cf63bfe14aa50
mohammedanaz <mohammedanaz@gmail.com>
2024-05-11 10:13:02 +0530

    basic off canvas Cart created for landing page(dynamic functions to be added...if successfull then same off canvas should be added to all page which has cart logo)

--- myfab/orders/urls.py
+++ myfab/orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import Cart
 
 
 urlpatterns = [
-   
+   path('cart/', Cart.as_view(), name = 'cart'),
 ]
--- myfab/orders/views.py
+++ myfab/orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.views import View
 
+########################## Cart View #################################
+class Cart(View):
 
+    def post(self, request):
+        product_id = request.POST.get('product_id')
+
+        print('product_id: ', product_id)
+
+        return redirect(reverse('main:products'))
+    
+    def get(self, request):
+        return render(request, 'cart.html')

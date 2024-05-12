7451c69b4e730eff24e9fe2cf2ce999247bbafca
mohammedanaz <mohammedanaz@gmail.com>
2024-05-12 21:50:49 +0530

    stitching measurement model deleted...and order_measurement field added to order model

--- myfab/orders/models.py
+++ myfab/orders/models.py
@@ -68,21 +68,13 @@ class Order(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     add_date = models.DateField(auto_now_add=True)
     payment_method = models.CharField(max_length=10, null=False, choices=PAYMENT_CHOICES)
-    status = models.CharField(max_length=10, null=False, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=10, null=False, choices=STATUS_CHOICES, default='P')
+    order_measurements = models.TextField(null=True)
+
 
     def __str__(self):
         return f"{self.pk}, {self.customer_id}"
     
-
-class StitchingMeasurement(models.Model):
-    '''
-    To store measurements details as a dictionary permenantly when order proceeds.
-    To pass the data to the stitching department.
-    '''
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    customer_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    order_measurements = models.TextField(null=False)
-
     def set_order_measurements(self, value):
         try:
             json_data = json.dumps(value)
@@ -96,3 +88,5 @@ class StitchingMeasurement(models.Model):
             return json_data
         except json.JSONDecodeError:
             return None
+    
+

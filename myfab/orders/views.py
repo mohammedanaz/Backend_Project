83659bf1d59237e5949ea73104d1d6777d54dbe0
mohammedanaz <mohammedanaz@gmail.com>
2024-05-14 20:59:45 +0530

    'Login required' msg displayed on product details page if user is not authenticated.

--- myfab/orders/views.py
+++ myfab/orders/views.py
@@ -20,57 +20,61 @@ class CartView(View):
         # Parse the JSON data sent from the frontend(product details page)
         data = json.loads(request.body)
         user = request.user
-        product_id = data.get('product_id')
-        product = Product.objects.get(pk=product_id)
-        ordertype = data.get('ordertype')
-        dresstype = data.get('dresstype')
-        price = data.get('price')
+        if user.is_authenticated:
+            product_id = data.get('product_id')
+            product = Product.objects.get(pk=product_id)
+            ordertype = data.get('ordertype')
+            dresstype = data.get('dresstype')
+            price = data.get('price')
 
-        # Cart save logics based on order types
-        if int(ordertype) == 1:
-            quantity = data.get('quantity-FO')
-            if validate_input(quantity):
+            # Cart save logics based on order types
+            if int(ordertype) == 1:
+                quantity = data.get('quantity-FO')
+                if validate_input(quantity):
+                    Cart.objects.create(
+                                        customer_id = user,
+                                        product_id = product,
+                                        order_type = int(ordertype),
+                                        qty = Decimal(quantity)
+                                        )
+                    return JsonResponse({'message': 'Cart created successfully.'})
+                else:
+                    error_message = f'Cart not saved. Quantity is not valid.'
+                    return JsonResponse({'error_message': error_message }, status=400)
+                
+            
+            elif int(ordertype) == 2:
+                quantity = data.get('quantity-FS')
+                if not validate_input(quantity):
+                    error_message = f'Cart not saved. Quantity is not valid.'
+                    return JsonResponse({'error_message': error_message }, status=400)
+                dresstype = Usage.objects.get(name=dresstype)
+                measurements_dict = {}
+                measurements_dict['customer_id'] = user.username
+                measurements_dict['dresstype'] = dresstype.name
+                for measurement in dresstype.measurements.all():
+                    measurement_name = measurement.name
+                    measurement_value = data.get(measurement_name)
+                    # Check if measurement_value is a valid number
+                    if validate_input(measurement_value):
+                        measurements_dict[measurement_name] = measurement_value
+                    else:
+                        error_message = f'Cart not saved. Measurement {measurement_name} is not valid.'
+                        return JsonResponse({'error_message': error_message }, status=400)
+                print(measurements_dict, quantity)
                 Cart.objects.create(
-                                    customer_id = user,
-                                    product_id = product,
-                                    order_type = int(ordertype),
-                                    qty = Decimal(quantity)
-                                    )
+                                        customer_id = user,
+                                        product_id = product,
+                                        order_type = int(ordertype),
+                                        qty = Decimal(quantity),
+                                        cart_measurements = measurements_dict,
+                                        )
                 return JsonResponse({'message': 'Cart created successfully.'})
             else:
-                error_message = f'Cart not saved. Quantity is not valid.'
+                error_message = 'Cart not saved. Order type not Known.'
                 return JsonResponse({'error_message': error_message }, status=400)
-            
-        
-        elif int(ordertype) == 2:
-            quantity = data.get('quantity-FS')
-            if not validate_input(quantity):
-                error_message = f'Cart not saved. Quantity is not valid.'
-                return JsonResponse({'error_message': error_message }, status=400)
-            dresstype = Usage.objects.get(name=dresstype)
-            measurements_dict = {}
-            measurements_dict['customer_id'] = user.username
-            measurements_dict['dresstype'] = dresstype.name
-            for measurement in dresstype.measurements.all():
-                measurement_name = measurement.name
-                measurement_value = data.get(measurement_name)
-                # Check if measurement_value is a valid number
-                if validate_input(measurement_value):
-                    measurements_dict[measurement_name] = measurement_value
-                else:
-                    error_message = f'Cart not saved. Measurement {measurement_name} is not valid.'
-                    return JsonResponse({'error_message': error_message }, status=400)
-            print(measurements_dict, quantity)
-            Cart.objects.create(
-                                    customer_id = user,
-                                    product_id = product,
-                                    order_type = int(ordertype),
-                                    qty = Decimal(quantity),
-                                    cart_measurements = measurements_dict,
-                                    )
-            return JsonResponse({'message': 'Cart created successfully.'})
         else:
-            error_message = 'Cart not saved. Order type not Known.'
+            error_message = 'Login required.'
             return JsonResponse({'error_message': error_message }, status=400)
     
 

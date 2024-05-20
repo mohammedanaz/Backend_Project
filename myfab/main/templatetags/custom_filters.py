4b910ebdd33eb9d20f6ef97a8b9e09ed7c2afe26
mohammedanaz <mohammedanaz@gmail.com>
2024-05-20 12:14:20 +0530

    basic request return accordion added to  user order page for delivered status (return submit logic to be implemented)

--- myfab/main/templatetags/custom_filters.py
+++ myfab/main/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-from main.models import CategoryChoice 
+from datetime import datetime, timedelta
 
 register = template.Library()
 
@@ -10,3 +10,14 @@ def filter_choices_by_category(choices_list, category):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def older_than_2_weeks(date):
+    '''
+    To check if the date is older than 2 weeks to dissable 
+    return request button.
+    '''
+    if not date:
+        return False
+    two_weeks_ago = datetime.now().date() - timedelta(weeks=2)
+    return date < two_weeks_ago
